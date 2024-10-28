0faf5a73413f7c6d6b0b66fd38aaa5c56bd3cb2e
Joao Goulart <joao.goulart@scoder.com.br>
2024-10-28 20:22:13 -0300

    feat: :sparkles: adds lista_1 reports

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportWidth: 1400,
   viewportHeight: 800,
+  reporter: "cypress-mochawesome-reporter",
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 });
